61fa71db56b63dd38f977729072b40c9c1de2c74
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2013-07-19 19:07:15 +0300

    v0.1.15

--- otrtalk.js
+++ otrtalk.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var OTRTALK_VERSION = "0.1.14";
+var OTRTALK_VERSION = "0.1.15";
 /*
     This program is free software; you can redistribute it and/or modify
     it under the terms of version 2 of the GNU General Public License as published by
