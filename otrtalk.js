c430990526481934299bf391894a78e8c94df358
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2014-11-12 03:43:45 +0300

    refactoring profiles - encapsulating loading of otr module and userfiles

--- otrtalk.js
+++ otrtalk.js
@@ -31,7 +31,7 @@ var fs_existsSync = fs.existsSync || path.existsSync;
 var os = require("os");
 var imapp = require("./lib/imapp.js");
 var tool = require("./lib/tool.js");
-var UI = {};
+var UI = require("./lib/ui.js");
 
 process.title = "otrtalk";
 
@@ -54,71 +54,6 @@ function shutdown(){
        process.exit();
     },300);
 }
-
-UI.accessKeyStore = function (profile,buddy,vfs,create,next){
-
-    if(!vfs){
-      return next(profile.openKeyStore(buddy));
-    }
-
-    /*
-      when using otr3-em and otr4-em otr modules we encrypt the files on the real file system
-      the AES 256bit encryption key and IV are derived from a password
-    */
-
-    if(fs_existsSync(profile.keys())){
-      //assume already encrypted from previous session.
-      //ask once for password.
-      program.password('enter key-store password: ', '', function(password){
-          next(profile.openKeyStore(buddy,vfs,password));
-      });
-      return;
-    }
-
-    if(create){
-      //first time double prompt for new password.
-      console.log("Your keys are stored in an encrypted key-store, protected with a password.");
-      console.log("** Pick a long password to protect your keys in case the key-store is stolen **");
-      program.password('new key-store password: ', '', function(password){
-      program.password('      confirm password: ', '', function(password_confirm){
-              if(password !== password_confirm){
-                  console.log("password mismatch!");
-                  next();
-              }else{
-                  next(profile.openKeyStore(buddy,vfs,password));
-              }
-           });
-      });
-      return;
-    }
-
-    next();
-};
-
-
-UI.ensureAccount = function (user,accountname,protocol,generate,next){
-  var fingerprint = user.fingerprint( accountname, protocol);
-
-  if(fingerprint){
-    return next('found');
-  }
-
-  if(generate){
-    console.log("Generating your OTR key...");
-    user.generateKey(accountname,protocol,function(err){
-      if(err){
-        next('error',err);
-      }else{
-        next('new');
-      }
-    });
-    return;
-  }
-
-  //account not found..
-  next('not-found');
-
-};
 
 (function(){
   var got_command = false;
@@ -178,7 +113,7 @@ UI.ensureAccount = function (user,accountname,protocol,generate,next){
         got_command = true;
         var cmd = require("./lib/commands/profiles.js");
         var _cmd = new cmd(UI);
-        _cmd.exec(action, profilename, id);
+        _cmd.exec(action, profilename || program.profile, id);
      });
 
   program
