6eba3c5a8a67a684b1e465e7d15e18a107688f5f
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2015-02-17 13:39:33 +0200

    refactoring

--- otrtalk.js
+++ otrtalk.js
@@ -22,40 +22,12 @@
 	ENet Networking Library is Copyright (c) 2002-2013 Lee Salzman
 */
 
-var commands = !module.parent ? require("./lib/commands") : undefined;
-
-if (commands) {
+if (!module.parent) {
 	process.title = "otrtalk";
 
-	//otrtalk being run as an application - process commands and options
-	commands.process(function (err, cmd) {
-		var mainCtrl;
-		if (err) {
-			console.log(err);
-			return;
-		}
-
-		if (cmd) {
-			mainCtrl = require("./lib/controllers/main.js");
-			mainCtrl.on("shutdown", function () {
-				console.log("exiting.");
-				process.exit();
-			});
-
-			cmd.exec(function (err, mode, config) {
-				if (err) {
-					console.log(err);
-				}
-				console.log("command finished.");
-				//command may have started services and is still running.
-				//when the command finishes its job it will call mainCtrl.exit() which
-				//will stop running services and shutdown the application.
-			}, mainCtrl);
-		} else {
-			console.log("You did not issue a command");
-			commands.help();
-		}
-	});
+	//otrtalk being run as an application
+	//start the main controller which processes commands and options
+	require("./lib/controllers/main.js").run();
 
 } else {
 	//otrtalk has been loaded as a module
